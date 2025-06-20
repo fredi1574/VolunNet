c6292e37db4b4c45b3a462181a6b32b15e766822
Fredi1574 <Fredi1574@gmail.com>
2025-06-20 20:03:31 +0300

    Fixed the marker not showing bug

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,24 +1,62 @@
 "use client";
+
+import type { LatLngExpression } from "leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import {
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
+
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
 
 export default function Map() {
+  const [mapCenter, setMapCenter] = useState<LatLngExpression>([0, 0]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setMapCenter([latitude, longitude]);
+    });
+  }, []);
+
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={mapCenter}
       zoom={13}
-      scrollWheelZoom={false}
-      className="mx-auto h-[700px] max-w-4/5 rounded-2xl shadow-2xl"
+      className="mx-auto h-[500px] w-4/5 rounded-xl shadow-lg"
+      key={mapCenter.toString()}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="Default Street Map">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </LayersControl.BaseLayer>
 
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+        <LayersControl.BaseLayer name="Stadia">
+          <TileLayer
+            attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
+
+      <Marker position={mapCenter}>
+        <Popup>You are here!</Popup>
       </Marker>
     </MapContainer>
   );
